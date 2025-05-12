c3e0132029864d39e4cb1d948e07b286c0d2474b
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-12 18:10:46 +0900

    no message

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Profile(props) {
+    return (
+        <>
+            <div>
+                확인용
+            </div>
+        </>
+    );
+}
+
+export default Profile;
