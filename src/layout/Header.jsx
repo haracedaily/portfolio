9b5205a97945832306d58c3e3c817f6ac31b1949
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-23 10:43:27 +0900

    no message

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,10 +4,10 @@ import styles from '../css/header.module.css'
 function Header(props) {
     return (
         <>
-            <header className={"sticky top-0 font-sans bg-white shadow whitespace-nowrap"}>
-                <div className={"flex flex-row gap-4 h-[10vh] justify-around items-center"}>
-                    <h3 className={styles.header_title+" text-2xl font-bold"}>최준호</h3>
-                    <ul className={styles.header_ul+" flex gap-3 text-gray-900"}>
+            <header className={"sticky top-0 font-sans bg-black shadow whitespace-nowrap"}>
+                <div className={"flex flex-row gap-4 h-[10vh] justify-around items-center text-shadow-fuchsia-500 text-shadow-2xs text-white"}>
+                    <h3 className={styles.header_title+" font-bold"}>최준호</h3>
+                    <ul className={styles.header_ul+" flex gap-3 "}>
                         <li>About me</li>
                         <li>Skill</li>
                         <li>Experience</li>
