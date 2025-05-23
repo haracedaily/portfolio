1de4bbc583fba4678676e2508fcc99f58681c7b5
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-23 11:12:56 +0900

    no message

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {Image} from "antd";
 
 function Profile(props) {
     return (
         <>
-            <div>
-                확인용
+            <div className={"flex flex-col gap-4 justify-center items-center"}>
+                <div>
+                    <Image></Image>
+                </div>
+                <div className={"flex flex-col gap-2 text-white"}>
+                    <div className={"flex gap-2 flex-row"}>
+                        <h4>이름</h4>
+                        <h4>최준호</h4>
+                    </div>
+                    <div>
+                        <h4></h4>
+                        <h4></h4>
+                    </div>
+                </div>
             </div>
         </>
     );
