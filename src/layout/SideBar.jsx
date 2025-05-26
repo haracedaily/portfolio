d0ad9403cff3ff606a30e83b31660c078cab3e3c
DESKTOP-29VI7GO\user <haracedaily>
2025-05-26 10:13:49 +0900

    no message

--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function SideBar(props) {
     return (
         <>
-            <div className={"absolute left-20 top-[20vh] w-[150px] bg-[rgba(255,255,255,0.5)]"}>
+ {/*           <div className={"absolute left-20 top-[20vh] w-[150px] bg-[rgba(255,255,255,0.5)]"}>
                     <ul>
                         li
                     </ul>
-            </div>
+            </div>*/}
         </>
     );
 }
