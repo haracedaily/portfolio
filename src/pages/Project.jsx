6aeaebc069aa8043339e6a9254dcf613200f0824
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-26 13:16:53 +0900

    no message

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from "../css/skill.module.css";
 
 function Project(props) {
     return (
         <>
-
+            <div id={"project"} className={"flex flex-col gap-4 justify-center items-center"}>
+                <div>
+                    <h3 className={styles.skill_title+" text-3xl font-bold"}>Project</h3>
+                </div>
+            </div>
         </>
     );
 }
