3bf0b7bedcfb1675b31f86c8a359ef1d1486e6ad
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-07-11 16:25:14 +0900

    no message

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -13,7 +13,7 @@ function Header(props) {
         <>
             <header className={`sticky top-0 font-sans shadow whitespace-nowrap ${props.backConfig==="dark"?"bg-black white":"bg-white black"}`}>
                 <div className={"flex flex-row gap-4 h-[10vh] justify-around items-center"}>
-                    <h3 className={styles.header_title+" font-bold"} style={{cursor:"pointer"}} onClick={()=>{props.setCurrentScroll(0);props.setScrollLoad(true);props.currentPosition.current=0;}}>최준호</h3>
+                    <h3 className={styles.header_title+" font-bold"} style={{cursor:"pointer"}} onClick={()=>{props.setCurrentScroll(0);props.setScrollLoad(true);props.currentPosition.current=0;}}>CJH's</h3>
                     <ul className={styles.header_ul+" flex gap-3 "}>
                         <li className={props.currentPosition.current===0?styles.choose_li:""} onClick={()=>{props.setCurrentScroll(0);props.setScrollLoad(true);props.currentPosition.current=0;}}>About me</li>
                         <li className={props.currentPosition.current===1?styles.choose_li:""} onClick={()=>{props.setCurrentScroll(mainHeight);props.setScrollLoad(true);props.currentPosition.current=1;}}>Skill</li>
