390abc4d7574112eb50e3b5e1b70cdd748c841dd
JH\nj700 <nj7001@naver.com>
2025-07-12 20:49:21 +0900

    no message

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react';
 import {Image} from "antd";
+import {useMediaQuery} from "react-responsive";
 
 function Profile(props) {
+    const isMobile = useMediaQuery({maxWidth: 900});
     let title_index=0;
     useEffect(() => {
         setInterval(()=>{
@@ -15,16 +17,16 @@ function Profile(props) {
 
     return (
         <>
-            <div id={"profile"} className={"flex flex-col gap-4 justify-center items-center"}>
+            <div id={"profile"} className={"flex flex-col gap-4 justify-center items-center"} style={{padding:"2rem"}}>
                 <div>
                     <Image></Image>
                 </div>
                 <div className={"flex flex-col gap-3 text-white"}>
-                    <div className={"flex flex-row gap-2"}>
-                        <h3 className={`text-2xl font-bold ${props.backConfig==="dark"?"white":"black"}`}>I'm</h3>
-                        <h3 className={"opacity-0 interval_title text-2xl text-amber-400 font-bold"}>JoonHo Choi</h3>
-                        <h3 className={"none opacity-0 interval_title text-2xl text-amber-400 font-bold"}>Web Developer</h3>
-                        <h3 className={"none opacity-0 interval_title text-2xl text-amber-400 font-bold"}>FullStack Developer</h3>
+                    <div className={`flex flex-row gap-2`}>
+                        <h3 className={`font-bold ${props.backConfig==="dark"?"white":"black"} ${isMobile?"text-lg":"text-2xl"}`}>I'm</h3>
+                        <h3 className={`opacity-0 interval_title text-amber-400 font-bold ${isMobile?"text-lg":"text-2xl"}`}>JoonHo Choi</h3>
+                        <h3 className={`none opacity-0 interval_title text-amber-400 font-bold ${isMobile?"text-lg":"text-2xl"}`}>Web Developer</h3>
+                        <h3 className={`none opacity-0 interval_title text-amber-400 font-bold ${isMobile?"text-lg":"text-2xl"}`}>FullStack Developer</h3>
                     </div>
                     <div className={`${props.backConfig==="dark"?"white":"black"}`}>
                         <h4>안녕하세요, 저는 최준호입니다.</h4>
@@ -32,7 +34,7 @@ function Profile(props) {
                         <h4>맡은 바 자부심을 가지고 도전할 수 있는 회사에서 일하고 싶습니다.</h4>
                     </div>
                     <div className={"flex gap-2 flex-row"}>
-                        <h3 className={`text-2xl font-bold ${props.backConfig==="dark"?"white":"black"}`}>Personal Info</h3>
+                        <h3 className={`${isMobile?"text-lg":"text-2xl"} font-bold ${props.backConfig==="dark"?"white":"black"}`}>Personal Info</h3>
                     </div>
                     <div className={"grid-cols-2 grid"}>
                         <div className={"grid grid-cols-[1fr_3fr]"}>
@@ -44,11 +46,11 @@ function Profile(props) {
                             <h4 className={"text-amber-500 font-bold text-shadow-2xs"}>1992.05</h4>
                         </div>
 
-                        <div className={"grid grid-cols-[1fr_3fr]"}>
+                        <div className={`grid ${isMobile?"col-span-2 grid-cols-[1fr_6fr]":"col-span-1 grid-cols-[1fr_3fr]"}`}>
                         <h4 className={`${props.backConfig==="dark"?"white":"black"}`}>Phone</h4>
-                        <h4 className={"text-amber-500 font-bold text-shadow-2xs"}>010-7368-5337</h4>
+                        <h4 className={`text-amber-500 font-bold text-shadow-2xs`}>010-7368-5337</h4>
                         </div>
-                        <div className={"grid grid-cols-[1fr_3fr]"}>
+                        <div className={`grid ${isMobile?"col-span-2 grid-cols-[1fr_6fr]":"col-span-1 grid-cols-[1fr_3fr]"}`}>
                             <h4 className={`${props.backConfig==="dark"?"white":"black"}`}>GitHub</h4>
                             <h4 className={"text-amber-500 font-bold cursor-pointer text-shadow-2xs"} onClick={()=>{window.open("https://github.com/haracedaily","_blank","location=no,menubar=no,status=no")}}>https://github.com/haracedaily</h4>
                         </div>
--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from '../css/skill.module.css'
+import {useMediaQuery} from "react-responsive";
 
 function Skill(props) {
+    const isMobile = useMediaQuery({maxWidth: 900});
     return (
         <>
             <div id={"skill"} className={`flex flex-col gap-4 justify-center items-center ${props.backConfig==="dark"?"white":"black"}`}>
                 <div>
                     <h3 className={styles.skill_title+" text-3xl font-bold"}>Skill's</h3>
                 </div>
-                <div style={{padding:"1rem"}} className={"flex flex-col gap-6 rounded-2xl border-[1px] border-amber-500"}>
-                    <div className={"flex flex-col gap-4 shadow-2xs"}>
-                        <h3 className={"font-bold text-3xl"}>Front-End</h3>
+                <div style={{padding:"1rem"}} className={`flex flex-col ${isMobile?"gap-2":"gap-6"} rounded-2xl border-[1px] border-amber-500`}>
+                    <div className={`flex flex-col ${isMobile?"gap-1":"gap-4"} shadow-2xs`}>
+                        <h3 className={`font-bold ${isMobile?"text-lg":"text-3xl"}`}>Front-End</h3>
                         <div className={"grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 skills"}>
                             <h3 className={"bg-amber-600"}>HTML</h3>
                             <h3 className={"bg-blue-400"}>CSS</h3>
@@ -22,8 +24,8 @@ function Skill(props) {
                         </div>
                     </div>
 
-                    <div className={"flex flex-col gap-4 shadow-2xs"}>
-                        <h3 className={"font-bold text-3xl"}>Back-End</h3>
+                    <div className={`flex flex-col ${isMobile?"gap-1":"gap-4"} shadow-2xs`}>
+                        <h3 className={`font-bold ${isMobile?"text-lg":"text-3xl"}`}>Back-End</h3>
                         <div className={"grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 skills"}>
                             <h3 className={"bg-green-300"}>Node.js</h3>
                             <h3 className={"bg-green-500"}>Express.js</h3>
@@ -33,8 +35,8 @@ function Skill(props) {
                         </div>
                     </div>
 
-                    <div className={"flex flex-col gap-4 shadow-2xs"}>
-                        <h3 className={"font-bold text-3xl"}>Database</h3>
+                    <div className={`flex flex-col ${isMobile?"gap-1":"gap-4"} shadow-2xs`}>
+                        <h3 className={`font-bold ${isMobile?"text-lg":"text-3xl"}`}>Database</h3>
                         <div className={"grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 skills"}>
                             <h3 className={"bg-cyan-900"}>MySQL</h3>
                             <h3 className={"bg-red-700"}>MsSQL</h3>
@@ -44,8 +46,8 @@ function Skill(props) {
                         </div>
                     </div>
 
-                    <div className={"flex flex-col gap-4 shadow-2xs"}>
-                        <h3 className={"font-bold text-3xl"}>Tools & Others</h3>
+                    <div className={`flex flex-col ${isMobile?"gap-1":"gap-4"} shadow-2xs`}>
+                        <h3 className={`font-bold ${isMobile?"text-lg":"text-3xl"}`}>Tools & Others</h3>
                         <div className={"grid grid-cols-[1fr_1fr_1fr_1fr] gap-2 skills"}>
                             <h3 className={"bg-gray-950 text-white"}>GitHub</h3>
                             <h3 className={"bg-gray-900 text-white"}>Notion</h3>
